9d094423855976711485371769929b8c31b02e2e
unknown <leonardo.1117@hotmail.com>
2020-06-18 22:34:18 -0300

    Updates

---
+++ precache-manifest.20296ad4fe7fb623e2ce15c830235cd5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035aeeb39a80c1d4cd29c94c9814d041",
+    "url": "/sem-processo-test/index.html"
+  },
+  {
+    "revision": "eef0bb5bf056675e3359",
+    "url": "/sem-processo-test/static/js/2.6fd66f94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sem-processo-test/static/js/2.6fd66f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bc4be3964bb1eee104",
+    "url": "/sem-processo-test/static/js/main.ffb1c7a7.chunk.js"
+  },
+  {
+    "revision": "cced24073197d8fe7e57",
+    "url": "/sem-processo-test/static/js/runtime-main.cc93a403.js"
+  },
+  {
+    "revision": "a245d124e4c6653d9829cc34b14bc153",
+    "url": "/sem-processo-test/static/media/Android.a245d124.svg"
+  },
+  {
+    "revision": "8620f13e708267849e0bc3d354227d89",
+    "url": "/sem-processo-test/static/media/Cisco.8620f13e.svg"
+  },
+  {
+    "revision": "a47ed92361f56c907f6e5fa3f2cbb9de",
+    "url": "/sem-processo-test/static/media/DirectorPhoto.a47ed923.svg"
+  },
+  {
+    "revision": "a1df6f465f5c77b73d62ca99f3b167bf",
+    "url": "/sem-processo-test/static/media/ImageBanner.a1df6f46.svg"
+  },
+  {
+    "revision": "7c95d0d07fc50b66ca7849537ff7bc31",
+    "url": "/sem-processo-test/static/media/MAQs.7c95d0d0.svg"
+  },
+  {
+    "revision": "a154b4db40d2b01ce2af7938b709dd6d",
+    "url": "/sem-processo-test/static/media/Medal.a154b4db.svg"
+  },
+  {
+    "revision": "d1d312e91ed7f931e713b1796d007b75",
+    "url": "/sem-processo-test/static/media/Oracle.d1d312e9.svg"
+  },
+  {
+    "revision": "254f2109630bac105392b34cd1ec5b6c",
+    "url": "/sem-processo-test/static/media/Python.254f2109.svg"
+  }
+]);
